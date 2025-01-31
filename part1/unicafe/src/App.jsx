7461337a76914152479dbcde25142520af68b225
root <root@LAPTOP-J70A2AVE>
2025-01-31 20:52:27 +0200

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,38 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good * 100) / all
+
+  const handleGood = () => {
+    console.log('good clicked')
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    console.log('neutral clicked')
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    console.log('bad clicked')
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={handleGood}>good</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
