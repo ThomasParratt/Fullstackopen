3957808c1b8994ec468e67a1a9d50a477eed1dbb
Thomas Parratt <thomasparratt@googlemail.com>
2025-08-09 11:54:28 +0300

    removed commented out old code

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,13 +1,5 @@
 const Header = (props) => <h1>{props.course}</h1>
 
-/*const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)*/
-
 const Content = (props) => (
   <div>
     {props.parts.map((part, id) => (
@@ -32,40 +24,6 @@ const Course = (props) => (
     </div>
 )
 
-/*const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      },
-    ],
-  }
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total
-        total={
-          course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises
-        }
-      />
-    </div>
-  )
-}*/
-
 const App = () => {
   const course = {
     id: 1,
@@ -85,6 +43,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
