68d9ca79ff16ec50da6820cb86b629f47d87cae4
Thomas Parratt <thomasparratt@googlemail.com>
2025-10-01 13:01:12 +0300

    3.6 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -61,6 +61,14 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'name or number missing' })
+  }
+
+  if (persons.some(p => p.name === body.name)) {
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
   const person = {
     name: body.name,
     number: body.number,
